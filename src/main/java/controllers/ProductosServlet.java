05ce1545bf0de672cc4109215f276a6f196bc0aa
BryanAntamba <bryjus2003@gmail.com>
2025-05-20 12:05:11 -0500

    Cambio login y css

--- src/main/java/controllers/ProductosServlet.java
+++ src/main/java/controllers/ProductosServlet.java
@@ -38,14 +38,13 @@ public class ProductosServlet extends HttpServlet{
 
         LoginService auth = new LoginServiceSessionImplement();
         Optional<String> usernameOptional= auth.getUserName(req);
-
+        boolean logeo = usernameOptional.isPresent();
         // 3. Configurar el tipo de contenido de la respuesta como HTML con UTF-8
         resp.setContentType("text/html;charset=UTF-8");
 
         // 4. Obtener objeto PrintWriter para escribir la respuesta HTML
         PrintWriter out = resp.getWriter();
 
-        if (usernameOptional.isPresent()) {
             // 5. Comenzar a generar la estructura HTML
             out.print("<!DOCTYPE html>");
             out.println("<html>");
@@ -58,9 +57,9 @@ public class ProductosServlet extends HttpServlet{
 
             // 6. Título principal de la página
             out.println("<h1>Lista de productos</h1>");
-
-            out.println("<h3>Hola querido admin sapin " + usernameOptional.get() + "</h3>");
-
+            if(logeo==true){
+                out.println("<h3>Hola querido " + usernameOptional.get() + "</h3>");
+            }
             // 7. Crear tabla para mostrar los productos
             out.println("<table border='1'>");  // Tabla con borde visible
 
@@ -69,53 +68,20 @@ public class ProductosServlet extends HttpServlet{
             out.println("<th>ID PRODUCTO</th>");
             out.println("<th>NOMBRE</th>");
             out.println("<th>CATEGORIA</th>");
-            out.println("<th>PRECIO</th>");
+            if(logeo==true){
+                out.println("<th>PRECIO</th>");
+            }
             out.println("</tr>");
 
             // 9. Iterar sobre la lista de productos y generar filas de la tabla
             productos.forEach(p -> {
                 out.println("<tr>");  // Nueva fila por cada producto
-                out.println("<td>"+p.getId()+"</td>");  // Celda con ID
-                out.println("<td>"+p.getNombre()+"</td>");  // Celda con nombre
-                out.println("<td>"+p.getTipo()+"</td>");  // Celda con categoría/tipo
-                out.println("<td>"+p.getPrecio()+"</td>");  // Celda con precio
-                out.println("</tr>");
-            });
-
-            // 10. Cerrar tabla y estructura HTML
-            out.println("</table>");
-            out.println("</body>");
-            out.println("</html>");
-        } else {
-            // 5. Comenzar a generar la estructura HTML
-            out.print("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<meta charset=\"utf-8\">");  // Especificar encoding
-            out.println("<link rel=\"stylesheet\" href=\"css/estilos.css\">");
-            out.println("<title>Lista de Productos</title>");  // Título de la pestaña
-            out.println("</head>");
-            out.println("<body>");
-
-            // 6. Título principal de la página
-            out.println("<h1>Lista de productos</h1>");
-
-            // 7. Crear tabla para mostrar los productos
-            out.println("<table border='1'>");  // Tabla con borde visible
-
-            // 8. Encabezados de la tabla
-            out.println("<tr>");  // Fila de encabezado
-            out.println("<th>ID PRODUCTO</th>");
-            out.println("<th>NOMBRE</th>");
-            out.println("<th>CATEGORIA</th>");
-            out.println("</tr>");
-
-            // 9. Iterar sobre la lista de productos y generar filas de la tabla
-            productos.forEach(p -> {
-                out.println("<tr>");  // Nueva fila por cada producto
-                out.println("<td>"+p.getId()+"</td>");  // Celda con ID
-                out.println("<td>"+p.getNombre()+"</td>");  // Celda con nombre
-                out.println("<td>"+p.getTipo()+"</td>");  // Celda con categoría/tipo
+                out.println("<td>" + p.getId() + "</td>");  // Celda con ID
+                out.println("<td>" + p.getNombre() + "</td>");  // Celda con nombre
+                out.println("<td>" + p.getTipo() + "</td>");  // Celda con categoría/tipo
+                if(logeo==true){
+                    out.println("<td>" + p.getPrecio() + "</td>");
+                }
                 out.println("</tr>");
             });
 
@@ -125,6 +91,5 @@ public class ProductosServlet extends HttpServlet{
             out.println("</html>");
         }
 
-
     }
-}
+
