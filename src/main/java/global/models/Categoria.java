441a6edb938584056ffb16be891a4312b259ed15
BryanAntamba <bryjus2003@gmail.com>
2025-05-27 11:00:29 -0500

    Error resuelto

--- src/main/java/global/models/Categoria.java
+++ src/main/java/global/models/Categoria.java
@@ -1,52 +1,54 @@
 package global.models;
 
 public class Categoria {
-    //JavaBeans
-    //1) Todas las variables deben estar encapsulados
-
-    private int idCategoria;
+    // Encapsulamiento usando wrappers para permitir null
+    private Integer idCategoria;
     private String nombre;
     private String descripcion;
     private int condicion;
 
-    //2) Generamos un objeto vacío porque no recibe parametros
-    public Categoria(){
-
+    // Constructor vacío
+    public Categoria() {
     }
 
-    public Categoria(int idCategoria, String nombre, String descripcion, int condicion) {
+    // Constructor con parámetros
+    public Categoria(Integer idCategoria, String nombre, String descripcion, int condicion) {
         this.idCategoria = idCategoria;
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.condicion = condicion;
     }
 
-    //Setters y Getters
-    public int getIdCategoria() {
+    // Getters y Setters
+    public Integer getIdCategoria() {
         return idCategoria;
     }
 
-    public void setIdCategoria(int idCategoria) {
+    public void setIdCategoria(Integer idCategoria) {
         this.idCategoria = idCategoria;
     }
 
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public String getDescripcion() {
         return descripcion;
     }
+
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
+
     public int getCondicion() {
         return condicion;
     }
+
     public void setCondicion(int condicion) {
         this.condicion = condicion;
     }
-
 }
